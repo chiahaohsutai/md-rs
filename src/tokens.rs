a6016e695a133577a60fe2ff68f2b6ac5447390b
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2024-12-11 22:15:24 -0500

    added tokenizer

--- src/tokens.rs
+++ src/tokens.rs
@@ -1,38 +1,95 @@
-const LOOKAHEAD: usize = 3;
-
+#[derive(PartialEq, Debug)]
 enum TokenType {
-    EXCLAMATION,
-    UNDERSCORE,
-    ASTERISK,
-    GREATER,
+    CHAR,
+	HASH,
+	STAR,
+	DASH,
+	BANG,
+    DIGIT,
     SPACE,
-    HASH,
-    DASH,
-    LBRACE,
-    RBRACE,
-    LPAREN,
+    PERIOD,
     RPAREN,
+	LPAREN,
+    NEWLINE,
+    GREATER,
+    RBRACKET,
+	LBRACKET,
     BACKTICK,
-    TRIPLETICK,
-    LINE,
-    EOF,
-    STR,
-    EQ,
-    ENUM,
+    UNDERSCORE,
 }
 
+impl TokenType {
+    fn from_char(c: char) -> TokenType {
+        match c {
+            '#' => TokenType::HASH,
+            '*' => TokenType::STAR,
+            '-' => TokenType::DASH,
+            '!' => TokenType::BANG,
+            '0'..='9' => TokenType::DIGIT,
+            ' ' => TokenType::SPACE,
+            '.' => TokenType::PERIOD,
+            '(' => TokenType::LPAREN,
+            ')' => TokenType::RPAREN,
+            '\n' => TokenType::NEWLINE,
+            '>' => TokenType::GREATER,
+            '[' => TokenType::LBRACKET,
+            ']' => TokenType::RBRACKET,
+            '`' => TokenType::BACKTICK,
+            '_' => TokenType::UNDERSCORE,
+            _ => TokenType::CHAR,
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
 pub struct Token {
-    token_type: TokenType,
-    value: Option<String>,
+    line: usize,
+    value: char,
+    kind: TokenType,
+    position: usize,
 }
 
 impl Token {
-    fn new<S: AsRef<str>>(token_type: TokenType, value: Option<S>) -> Token {
-        let value = value.map(|s| s.as_ref().to_string());
-        Token { token_type, value }
+    fn new(value: char, position: usize, line: usize) -> Token {
+        let kind = TokenType::from_char(value);
+        Token { value, kind, position, line }
     }
 }
 
 pub fn tokenize<S: AsRef<str>>(input: S) -> Vec<Token> {
-    todo!();
+    let mut tokens = Vec::new();
+    let mut line = 0;
+    let mut position = 0;
+    for c in input.as_ref().chars() {
+        tokens.push(Token::new(c, position, line));
+        if c == '\n' {
+            line += 1;
+            position = 0;
+            continue;
+        }
+        position += 1;
+    }
+    tokens
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tokenize() {
+        let input = "## Hi!,\n ";
+        let expected = vec![
+            Token::new('#', 0, 0),
+            Token::new('#', 1, 0),
+            Token::new(' ', 2, 0),
+            Token::new('H', 3, 0),
+            Token::new('i', 4, 0),
+            Token::new('!', 5, 0),
+            Token::new(',', 6, 0),
+            Token::new('\n', 7, 0),
+            Token::new(' ', 0, 1),
+        ];
+        assert_eq!(tokenize(input), expected);
+    }
 }
